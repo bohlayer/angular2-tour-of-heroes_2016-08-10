b8d01d87532d99906322ad72eaa1c39934570955
Stephen Bohlayer <stephen.bohlayer@gmail.com>
2016-08-10 15:28:34 -0400

    completed 5 min quickstart

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Hello World<br>My First Angular 2 App</h1>
+    <h3><br>https://angular.io/docs/ts/latest/quickstart.html (as of 2016-08-10)</h3>
+  `
+})
+export class AppComponent { }
