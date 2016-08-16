f754e4f28eda88e2da442c74d1d947fa3d713645
Stephen Bohlayer <stephen.bohlayer@gmail.com>
2016-08-16 19:38:22 -0400

    Completed Tutorial Section "Services"

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
+
+import { HeroService } from './hero.service';
 
 
 @Component({
@@ -67,29 +69,30 @@ import { Hero } from './hero';
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 
 })
-export class AppComponent { 
+export class AppComponent implements OnInit{ 
   
   title = 'Tour of Heroes';
 
   selectedHero: Hero;
 
-  public heroes = HEROES;
+  heroes: Hero[];
+
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    //this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
---
+++ app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+//import of Hero only needed if including getHeroesSlowly method
+import { Hero } from './hero';
+
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+
+  //getHeroesSlowly simulates a slow connection //called from app.component.ts
+  getHeroesSlowly() {
+  return new Promise<Hero[]>(resolve =>
+    setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+  );
+}
+
+}
